51bc75dd24ad1108c907367ab515259c93a6d0b2
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-04-09 19:51:38 +0700

    refactor: add email and location

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ const AddressBook = [
     fullName: "Mochamad Irvan",
     age: 20,
     phone: "08123456789",
+    email: "mchmdirvaan@gmail.com",
+    location: "Jakarta",
   },
   {
     id: 2,
     fullName: "Irvan Mochamad",
     age: 10,
     phone: "0881080070700",
+    email: "irvaanmchmd@gmail.com",
+    location: "Bandung",
   },
 ];
 
