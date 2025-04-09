d9f0cb814efc2a92ad2017ab59911439c8bb519f
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-04-09 19:45:36 +0700

    feat: add dummy data on console

---
+++ script.js
@@ -0,0 +1,16 @@
+const fruits = [
+  {
+    name: "Apple",
+    color: "red",
+    isFavorite: true,
+    tags: ["delicios", "legendary"],
+  },
+  {
+    name: "Banana",
+    color: "blue",
+    isFavorite: false,
+    tags: ["legendary"],
+  },
+];
+
+console.log("fruits:", fruits);
