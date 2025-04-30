f162ab8db68bff7984f127f7e9f1f7570a645f41
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-04-30 19:29:56 +0700

    refactor: remove 2 contact entries

--- script.js
+++ script.js
@@ -9,27 +9,11 @@ let dataContacts = [
   },
   {
     id: 2,
-    fullName: "Irvan Mochamad",
-    age: 10,
-    phone: "+62-852-1955-7750",
-    email: "irvaanmchmd@gmail.com",
-    city: "Bandung",
-  },
-  {
-    id: 3,
     fullName: "Prabowo Subianto",
     age: 70,
     phone: "+62-812-3456-7890",
     email: "prabowo@gmail.com",
     city: "Bogor",
-  },
-  {
-    id: 4,
-    fullName: "Gibran Rakabuming Raka",
-    age: 40,
-    phone: "+62-812-3333-4444",
-    email: "gibran@gmail.com",
-    city: "Solo",
   },
 ];
 
