699ac81ff726011e7b273417c90afb2b77c0f137
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-05-28 11:23:42 +0700

    feat: display contact on console

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const dataContacts = [
+  {
+    id: 1,
+    fullname: "Adhitya Sofyan",
+    phone: "+6281280807070",
+    email: "adhitya@gmail.com",
+    city: "Yogyakarta",
+    birthdate: new Date("1977/11/06"),
+  },
+];
+
+console.log(dataContacts);
