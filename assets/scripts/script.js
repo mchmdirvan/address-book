11d27f9459d2e5bc5e79d8cdca6aa7e49a147c8f
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-05-30 09:28:43 +0700

    style: change padding and font size

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -176,11 +176,11 @@ function renderContacts() {
   contactListElement.innerHTML = contacts
     .map((contact) => {
       return `
-      <tr class="shadow-sm cursor-pointer">
-        <td class="ps-20 py-2">${contact.fullname}</td>
+      <tr class="shadow-sm cursor-pointer text-sm">
+        <td class="ps-20 py-6">${contact.fullname}</td>
         <td>${contact.email}</td>
         <td>${contact.phone}</td>
-        <td class="flex gap-5 p-2">
+        <td class="flex gap-5 justify-center p-3">
           <button class="p-2 hover:bg-slate-200 rounded-full cursor-pointer">
           <svg class="max-w-4 max-h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
         
