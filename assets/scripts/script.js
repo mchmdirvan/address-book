cc767994d34da0834a8c4c10feea1a7bac464755
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-05-30 09:21:00 +0700

    style: cursor pointer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -176,19 +176,19 @@ function renderContacts() {
   contactListElement.innerHTML = contacts
     .map((contact) => {
       return `
-      <tr class="shadow-sm">
-      <td class="ps-20 py-2">${contact.fullname}</td>
-      <td>${contact.email}</td>
-      <td>${contact.phone}</td>
-      <td class="flex gap-5 p-2">
-        <button class="p-2 hover:bg-slate-200 rounded-full">
-        <svg class="max-w-4 max-h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
-      
-        </button>
-        <button class="p-2 hover:bg-slate-200 rounded-full">
-        <svg class="max-w-4 max-h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
-        </button>
-      </td>
+      <tr class="shadow-sm cursor-pointer">
+        <td class="ps-20 py-2">${contact.fullname}</td>
+        <td>${contact.email}</td>
+        <td>${contact.phone}</td>
+        <td class="flex gap-5 p-2">
+          <button class="p-2 hover:bg-slate-200 rounded-full cursor-pointer">
+          <svg class="max-w-4 max-h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
+        
+          </button>
+          <button class="p-2 hover:bg-slate-200 rounded-full cursor-pointer">
+          <svg class="max-w-4 max-h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
+          </button>
+        </td>
       </tr>
     `;
     })
