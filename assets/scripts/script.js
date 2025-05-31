f06c0a28630583802da20e9858b7f5b677959b6e
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-05-31 14:20:13 +0700

    refactor: ui

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -61,7 +61,7 @@ function renderContacts() {
     .map((contact) => {
       return `
         <tr
-          class="shadow-sm cursor-pointer text-sm hover:bg-slate-100 transition-all duration-500"
+          class="shadow-sm text-sm hover:bg-slate-100 transition-all duration-500"
         >
           <td class="ps-20 py-6">${contact.fullname}</td>
           <td>${contact.email}</td>
@@ -79,13 +79,15 @@ function renderContacts() {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-trash-icon lucide-trash"
+                class="lucide lucide-eye-icon lucide-eye"
               >
-                <path d="M3 6h18" />
-                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                <path
+                  d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
+                />
+                <circle cx="12" cy="12" r="3" />
               </svg>
             </button>
+            
             <button class="p-2 hover:bg-slate-200 rounded-full cursor-pointer">
               <svg
                 class="max-w-4 max-h-4"
@@ -104,6 +106,26 @@ function renderContacts() {
                   d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
                 />
                 <path d="m15 5 4 4" />
+              </svg>
+            </button>
+
+            <button class="p-2 hover:bg-slate-200 rounded-full cursor-pointer">
+              <svg
+                class="max-w-4 max-h-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-trash-icon lucide-trash"
+              >
+                <path d="M3 6h18" />
+                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
               </svg>
             </button>
           </td>
