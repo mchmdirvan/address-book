9eff70dbfd9479a618728ef6d37ed3334021e1cf
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-01 11:31:40 +0700

    feat: implement contact form submission handling

---
+++ create/script.js
@@ -0,0 +1,25 @@
+const contactFormElement = document.getElementById("contact-form");
+
+function addContact(event) {
+  event.preventDefault();
+  const contacts = loadContacts();
+
+  const contactFormData = new FormData(contactFormElement);
+
+  const newId = contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 1;
+
+  const newContact = {
+    id: newId,
+    fullname: contactFormData.get("fullname"),
+    phone: contactFormData.get("phone"),
+    email: contactFormData.get("email"),
+    city: contactFormData.get("city"),
+    birthdate: contactFormData.get("birthdate"),
+    // isFavorited: contactFormData.get("isFavorited"),
+  };
+
+  const updateContacts = [...contacts, newContact];
+  saveContacts(updateContacts);
+}
+
+contactFormElement.addEventListener("submit", addContact);
