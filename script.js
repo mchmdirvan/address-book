4c53673a539dd1e337106e61a5701690a88224e6
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-01 11:33:35 +0700

    refactor: move initial contact data to storage.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-let dataContacts = [
-  {
-    id: 1,
-    fullname: "Adhitya Sofyan",
-    phone: "+6281280807070",
-    email: "adhitya@gmail.com",
-    city: "Yogyakarta",
-    birthdate: new Date("1977/11/06"),
-    isFavorited: true,
-  },
-  {
-    id: 2,
-    fullname: "Raditya Dika",
-    phone: "+6281234567890",
-    email: "raditya@gmail.com",
-    city: "Jakarta",
-    birthdate: new Date("1984/12/28"),
-    isFavorited: false,
-  },
-  {
-    id: 3,
-    fullname: "Aryo Sheggario",
-    phone: "+6281987654321",
-    email: "aryo@gmail.com",
-    city: "Bandung",
-    birthdate: new Date("1990/03/15"),
-    isFavorited: true,
-  },
-];
-
 // ------------------------------------------------------------------
 // DOM
 // ------------------------------------------------------------------
--- storage.js
+++ storage.js
@@ -1,6 +1,32 @@
-// ------------------------------------------------------------------
-// STORAGE
-// ------------------------------------------------------------------
+let dataContacts = [
+  {
+    id: 1,
+    fullname: "Adhitya Sofyan",
+    phone: "+6281280807070",
+    email: "adhitya@gmail.com",
+    city: "Yogyakarta",
+    birthdate: new Date("1977/11/06"),
+    isFavorited: true,
+  },
+  {
+    id: 2,
+    fullname: "Raditya Dika",
+    phone: "+6281234567890",
+    email: "raditya@gmail.com",
+    city: "Jakarta",
+    birthdate: new Date("1984/12/28"),
+    isFavorited: false,
+  },
+  {
+    id: 3,
+    fullname: "Aryo Sheggario",
+    phone: "+6281987654321",
+    email: "aryo@gmail.com",
+    city: "Bandung",
+    birthdate: new Date("1990/03/15"),
+    isFavorited: true,
+  },
+];
 
 function saveContacts(contacts) {
   localStorage.setItem("data-contacts", JSON.stringify(contacts));
@@ -10,6 +36,7 @@ function loadContacts() {
   const contacts = localStorage.getItem("data-contacts");
   if (!contacts) {
     saveContacts(dataContacts);
+    renderContacts();
   }
   try {
     return JSON.parse(contacts);
