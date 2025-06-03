a5a4a6055ce8982a81ca48907b64facad0b5d3c3
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-03 15:49:20 +0700

    refactor: simplify by moving getID and formatted date in utils

--- edit/script.js
+++ edit/script.js
@@ -7,46 +7,27 @@ const isFavoritedElement = document.getElementById("isFavorited");
 
 const contactFormElement = document.getElementById("contact-form");
 
-function getID() {
-  const queryString = window.location.search;
-  const params = new URLSearchParams(queryString);
-  const id = params.get("id");
+const contacts = loadContacts();
+const id = getID();
 
-  return id;
-}
-
-function getContact() {
-  const contacts = loadContacts();
-  const id = getID();
-
-  const setContact = contacts.find((contact) => {
-    if (id == contact.id) {
-      fullnameInputElement.value = contact.fullname;
-      phoneInputElement.value = contact.phone;
-      emailInputElement.value = contact.email;
-      cityInputElement.value = contact.city;
-      birthdateInputElement.value = new Date(contact.birthdate)
-        .toISOString()
-        .split("T")[0];
-      isFavoritedElement.checked = Boolean(contact.isFavorited);
-    }
-  });
-
-  return setContact;
-}
+const setContact = contacts.find((contact) => {
+  if (id == contact.id) {
+    fullnameInputElement.value = contact.fullname;
+    phoneInputElement.value = contact.phone;
+    emailInputElement.value = contact.email;
+    cityInputElement.value = contact.city;
+    birthdateInputElement.value = new Date(contact.birthdate)
+      .toISOString()
+      .split("T")[0];
+    isFavoritedElement.checked = Boolean(contact.isFavorited);
+  }
+});
 
 function updateContact(event) {
   event.preventDefault();
-  const contacts = loadContacts();
-  const id = getID();
 
   const contactFormData = new FormData(contactFormElement);
-
-  const birthdate = new Date(contactFormData.get("birthdate"));
-  const formattedDate = new Intl.DateTimeFormat("id-ID", {
-    dateStyle: "long",
-    timeZone: "Asia/Jakarta",
-  }).format(birthdate);
+  formattedDate = formattedDate(contactFormData);
 
   const newContact = {
     id: Number(id),
@@ -70,9 +51,7 @@ function updateContact(event) {
   });
 
   saveContacts(updatedContacts);
-
   window.location.href = "/";
 }
 
-getContact();
 contactFormElement.addEventListener("submit", updateContact);
--- utils.js
+++ utils.js
@@ -5,3 +5,21 @@ function getParams() {
 
   return keyword;
 }
+
+function getID() {
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
+
+  return id;
+}
+
+function formattedDate(contactFormData) {
+  const birthdate = new Date(contactFormData.get("birthdate"));
+  const formattedDate = new Intl.DateTimeFormat("id-ID", {
+    dateStyle: "long",
+    timeZone: "Asia/Jakarta",
+  }).format(birthdate);
+
+  return formattedDate;
+}
